091bb1ae14e0ca4356afa5dd7125a389c8e29512
EthanKatungi <katungiethan@gmail.com>
2021-10-11 11:18:26 +0300

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/painting.dart';
-
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: const [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage('images/sarah.png'),
-              ),
-              Text(
-                'Ethan Katungi',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'DancingScript',
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage(
+                    'images/sarah.png',
+                  ),
                 ),
-              ),
-              Text(
-                'Student',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.tealAccent,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'SourceSansPro',
-                  letterSpacing: 2.5,
+                Text(
+                  'Sarah Conor',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'DancingScript',
+                  ),
                 ),
-              ),
-            ],
+                Text(
+                  'Terminator Killer',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal[100],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
